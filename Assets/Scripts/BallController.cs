bbc592b822688205842a4571b7d2a63254ad6651
srieck213 <srieck213@gmail.com>
2022-01-29 18:43:04 -0600

    configured paddle and ball
    set up paddle and ball scripts, added walls with colliders, and still need to add bricks, actions, and game master

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public bool ballLaunched = false;
+    public Rigidbody2D ballRigidbody;
+    public Vector2[] startDirections;
+    public int randomNumber;
+    public float ballForce;
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !ballLaunched)
+        {
+            //launch ball
+            randomNumber = Random.Range(0, startDirections.Length);
+            ballRigidbody.AddForce(startDirections[randomNumber] * ballForce, ForceMode2D.Impulse);
+            ballLaunched = true;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "DefeatZone")
+        {
+            ballRigidbody.velocity = Vector3.zero;
+            transform.position = startPosition;
+            ballLaunched = false;
+        }
+    }
+}
